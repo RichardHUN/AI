8450c681a9a9a035f709c21138710a8fe2e6ad0c
Kasztl Richard <richardhun@mailbox.unideb.hu>
2025-03-27 19:35:27 +0100

    Prettified the code. Changed trial-error to show averages.

--- NQueens/Main.py
+++ NQueens/Main.py
@@ -2,18 +2,6 @@ from Base.Search import breadth_first_tree_search, breadth_first_tree_search_wit
     depth_first_graph_search_with_stepcount, astar_search, astar_search_with_stepcount, trial_error_with_stepcount
 from NQueens import NQueens
 
-nq4 = NQueens(4)
-
-b = breadth_first_tree_search_with_stepcount(nq4)
-print( "Number of steps needed for width search: ", b[1],
-       "\nNumber of node discovered by width search: ", b[2], "\n")
-d = depth_first_graph_search_with_stepcount(nq4)
-print( "Number of steps needed for depth search: ", d[1],
-       "\nNumber of node discovered by depth search: ", d[2], "\n")
-te = trial_error_with_stepcount(nq4)
-print( "Number of steps needed for trial-error search: ", te[1],
-       "\nNumber of node discovered by trial-error search: ", te[2], "\n")
-
 def sort_by_heur(items):
     """Válasszuk mindig a lehető legnagyobb indexű sort"""
     return sorted(items, key=lambda x: sum(x.state))
@@ -27,6 +15,33 @@ def nrOfX(matrix, X):
         if element == X:
             count +=1
     return count
+
+nq4 = NQueens(4)
+
+limit = 10000
+steps = 0
+discovered = 0
+notFound = 0
+for i in range(limit):
+    te = trial_error_with_stepcount(nq4)
+    if te[1] != -1:
+        steps += te[1]
+        discovered += te[2]
+        continue
+    notFound += 1
+avgSteps = steps / ( limit - notFound )
+avgDiscovered = discovered / ( limit - notFound )
+
+print( "Average number of steps needed for trial-error search(", limit, " tries) : ", avgSteps.__int__(),
+       "\nAverage number of nodes discovered by trial-error search(", limit, " tries) : ", avgDiscovered.__int__(),
+       "\nNumber of times trial-error search didn't find the goal(", limit, " tries) : ", notFound, "\n")
+
+b = breadth_first_tree_search_with_stepcount(nq4)
+print( "Number of steps needed for width search: ", b[1],
+       "\nNumber of node discovered by width search: ", b[2], "\n")
+d = depth_first_graph_search_with_stepcount(nq4)
+print( "Number of steps needed for depth search: ", d[1],
+       "\nNumber of node discovered by depth search: ", d[2], "\n")
 
 a = astar_search_with_stepcount(nq4, sort_by_number_of_twos)
 print( "Number of steps needed for astar search(nrOfTwos heuristic): ", a[1],
